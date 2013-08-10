a0ae2866804b6d228eca0a6973ddaae40e2d1a7f
Kent Huang <kentwelcome@gmail.com>
2013-08-10 17:47:06 +0800

    Add crawler.py
    Parse data with given URL

---
+++ src/crawler.py
@@ -0,0 +1,101 @@
+# encoding: utf-8
+import os
+import re
+import sys
+import urllib
+import json
+import codecs
+from bs4 import BeautifulSoup
+
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+
+DEBUG = 1
+CAHCE_FILE = "./dump.log"
+FETCH_URL = 'http://www.cy.gov.tw/sp.asp?xdUrl=.%2FDI%2Fedoc%2Fdb2.asp&ctNode=911&edoc_no=2&doQuery=1&intYear=&mm=&input_edoc_no=&case_pty=&input_edoc_unit=&keyword=&submit=%E6%9F%A5%E8%A9%A2'
+DOWNLOAD_FOLDER = '../data/'
+
+def getDomain(url):
+    reObj = re.match(r'(http://.+/)',url)
+    return reObj.group()
+
+def fetchPage(url):
+    if DEBUG and os.path.exists(CAHCE_FILE):
+        rc = fetchPageFromFile(CAHCE_FILE)
+    else:
+        rc = fetchPageFromURL(url)
+    return rc
+
+def fetchPageFromURL(url):
+    return urllib.urlopen(url).read()
+
+def fetchPageFromFile(file):
+    fd = open(file,'r')
+    return fd.read()
+
+def contentDownloader(url):
+    pass
+
+def contentParser(content):
+    soup = BeautifulSoup(content)
+    talbe = parseCaseTable(soup)
+    return talbe
+
+def normalizeContent(content):
+    content = content.encode('utf-8')
+    content = content.replace(' ', '')
+    content = content.replace('\t', '')
+    content = content.replace('\n', '')
+    return content
+
+def insertCase(caseTable, content, index):
+    item = index%6
+    caseNo = index/6
+    if item == 0:
+        caseTable[caseNo] = {"date":content.text}
+        pass
+    elif item == 1:
+        caseTable[caseNo]['id'] = normalizeContent(content.text)
+        pass
+    elif item == 2: 
+        caseTable[caseNo]['describe'] = normalizeContent(content.text)
+        pass
+    elif item == 3:
+        caseTable[caseNo]['docx'] = FETCH_DOMAIN + normalizeContent(content.a['href'])
+        pass
+    elif item == 4:
+        caseTable[caseNo]['pdf'] = ''
+        pass
+    elif item == 5:
+        caseTable[caseNo]['reportLink'] = FETCH_DOMAIN + normalizeContent(content.a['href'])
+        pass
+    pass
+
+def parseCaseTable(parser):
+    cases = parser.find('div',attrs={'class':'lpTb'}).find_all('td')
+    caseNum = len(cases)/6
+    caseTable = [None]*caseNum
+    idx = 0
+    for case in cases:
+        insertCase(caseTable, case, idx)
+        idx += 1
+    return caseTable
+
+def dumpToJson(table):
+    fd = codecs.open(os.path.join(DOWNLOAD_FOLDER,'data.json'),'w',encoding="utf-8")
+    json.dump(table,fd,indent=2,ensure_ascii=False)
+    fd.close()
+    pass
+
+def main(argv):
+    content = fetchPage(FETCH_URL)
+    table = contentParser(content)
+    dumpToJson(table)
+    pass
+
+if __name__ == '__main__':
+    FETCH_DOMAIN = getDomain(FETCH_URL)
+    main(sys.argv)
