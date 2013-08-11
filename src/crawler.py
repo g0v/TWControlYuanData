1e7bc85b1d2f2ed46974d75401037e720f19244a
Kent Huang <kentwelcome@gmail.com>
2013-08-12 01:02:45 +0800

    Atuo download all the page

--- src/crawler.py
+++ src/crawler.py
@@ -19,11 +19,14 @@ if sys.getdefaultencoding() != default_encoding:
     sys.setdefaultencoding(default_encoding)
 
 
-DEBUG = True
+DEBUG = False
 IS_DUMP_FILE = False
 CAHCE_FILE = "./dump.log"
 FETCH_URL = 'http://www.cy.gov.tw/sp.asp?xdUrl=.%2FDI%2Fedoc%2Fdb2.asp&ctNode=911&edoc_no=2&doQuery=1&intYear=&mm=&input_edoc_no=&case_pty=&input_edoc_unit=&keyword=&submit=%E6%9F%A5%E8%A9%A2'
 DOWNLOAD_FOLDER = '../data/'
+DOCUMENT_FOLDER = '../data/doc'
+
+CONTROL_YUAN_URL = "http://www.cy.gov.tw/sp.asp?xdURL=./di/edoc/db2.asp&ctNode={0:d}&doQuery=1&cPage={1:d}&edoc_no=2&intYear="
 
 def getDomain(url):
     reObj = re.match(r'(http://.+/)',url)
@@ -48,13 +51,24 @@ def fetchPageFromFile(file):
     fd = open(file,'r')
     return fd.read()
 
-def contentDownloader(url):
-    pass
+def contentDownloader(caseType, page):
+    url = CONTROL_YUAN_URL.format(caseType,page)
+    content = fetchPage(url)
+    return content
 
-def contentParser(content):
-    soup = BeautifulSoup(content)
-    talbe = parseCaseTable(soup)
+def createParser(content):
+    return BeautifulSoup(content)
+
+def caseParser(parser, content):
+    talbe = parseCaseTable(parser)
     return talbe
+
+def pageParser(parser, content):
+    pageNum = parsePageNumber(parser)
+    return pageNum
+
+def page():
+    pass
 
 def normalizeContent(content):
     content = content.encode('utf-8')
@@ -77,15 +91,30 @@ def insertCase(caseTable, content, index):
         caseTable[caseNo]['describe'] = normalizeContent(content.text)
         pass
     elif item == 3:
-        caseTable[caseNo]['docx'] = FETCH_DOMAIN + normalizeContent(content.a['href'])
-        pass
+        try:
+            caseTable[caseNo]['docx'] = FETCH_DOMAIN + normalizeContent(content.a['href'])
+        except:
+            caseTable[caseNo]['docx'] = ''
     elif item == 4:
-        caseTable[caseNo]['pdf'] = ''
-        pass
+        try:
+            caseTable[caseNo]['pdf'] = FETCH_DOMAIN + normalizeContent(content.a['href'])
+        except:
+            caseTable[caseNo]['pdf'] = ''
     elif item == 5:
-        caseTable[caseNo]['reportLink'] = FETCH_DOMAIN + normalizeContent(content.a['href'])
-        pass
+        try:
+            caseTable[caseNo]['reportLink'] = FETCH_DOMAIN + normalizeContent(content.a['href'])
+        except Exception, e:
+            caseTable[caseNo]['reportLink'] = ''
     pass
+
+def parsePageNumber(parser):
+    try:
+        lastPage = parser.find('div',attrs={'class':'page'}).find_all('a')[-1]['href']
+        reObj = re.match(r'.*cPage=(\d+).*',lastPage)
+        return int(reObj.group(1))
+    except Exception, e:
+        return 0
+
 
 def parseCaseTable(parser):
     cases = parser.find('div',attrs={'class':'lpTb'}).find_all('td')
@@ -97,16 +126,34 @@ def parseCaseTable(parser):
         idx += 1
     return caseTable
 
-def dumpToJson(table):
-    fd = codecs.open(os.path.join(DOWNLOAD_FOLDER,'data.json'),'w',encoding="utf-8")
+def dumpToJson(table,caseType,page):
+    fileName = "data_{0:d}_{1:d}.json".format(caseType,page)
+    fd = codecs.open(os.path.join(DOWNLOAD_FOLDER,fileName),'w',encoding="utf-8")
     json.dump(table,fd,indent=2,ensure_ascii=False)
     fd.close()
     pass
 
+def crawlerByType(caseType):
+    idx = 1
+
+    # First time
+    print "Download page: {0:d}".format(idx)
+    content = contentDownloader(caseType,idx)
+    parser = createParser(content)
+    pageNum = pageParser(parser,content)
+    table = caseParser(parser, content)
+    dumpToJson(table,caseType,idx)
+
+    for idx in xrange(2,pageNum+1):
+        print "Download page: {0:d}".format(idx)
+        content = contentDownloader(caseType,idx)
+        parser = createParser(content)
+        table = caseParser(parser, content)
+        dumpToJson(table,caseType,idx)
+    pass
+
 def main(argv):
-    content = fetchPage(FETCH_URL)
-    table = contentParser(content)
-    dumpToJson(table)
+    crawlerByType(911)
     pass
 
 if __name__ == '__main__':
